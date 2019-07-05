28f88f25d5c86b407a3fbff052eebaf751aeec8a
setiawanswan <setiawan.tumilin@gmail.com>
2019-07-05 22:19:35 +0700

    learn regex

--- class.js
+++ class.js
@@ -1,36 +1,39 @@
-// let rectangle = class{
-//     constructor(height, width){
-//         this.height = heigth;
-//         this.width = width;
-//     }
-// };
-// console.log(rectangle.name);
+// This is unnamed class
+let rectangle = class{
+    constructor(height, width){
+        this.height = height;
+        this.width = width;
+    }
+}
 
-// let rectangle = class rectangle2{
-//     constructor(heigth, width){
-//         this.height = heigth;
-//         this.width = width;
+console.log(rectangle.name)
 
-//     }
-// };
+//This is call named class
+let shape = class shape{
+    constructor(height, width){
+        this.height = height;
+        this.width = width;
+    }
+}
 
-// console.log(rectangle.name);
+console.log(shape.name)
 
 class triangle{
     constructor(height, width){
         this.height = height;
         this.width = width;
     }
+
     get area(){
-        return this.calculationArea();
+        return this.calculationArea()
     }
+
     calculationArea(){
         return this.height * this.width;
     }
-
 }
-const square = new triangle(10, 10);
-console.log(square.area);
+const value = new triangle(10, 10)
+console.log(value.area)
 
 class animal {
     constructor(x,y){
